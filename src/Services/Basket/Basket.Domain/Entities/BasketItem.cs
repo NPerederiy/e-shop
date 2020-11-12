88ac7d7cf225a7309a77457dd5832404328daf81
NPerederiy <nick.perederiy@gmail.com>
2020-11-13 01:48:48 +0200

    F-0002 Added domain entities

---
+++ src/Services/Basket/Basket.Domain/Entities/BasketItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Basket.Domain.Entities
+{
+    public class BasketItem
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal OldUnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public string PictureUrl { get; set; }
+    }
+}
---
+++ src/Services/Basket/Basket.Domain/Entities/CustomerBasket.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basket.Domain.Entities
+{
+    public class CustomerBasket
+    {
+        public Guid BuyerId { get; set; }
+        public IEnumerable<BasketItem> Items { get; set; }
+    }
+}
