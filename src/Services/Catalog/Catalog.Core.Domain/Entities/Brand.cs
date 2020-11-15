263df5a04f83f2acd692709822da9637fb164768
NPerederiy <nick.perederiy@gmail.com>
2020-11-15 18:41:11 +0200

    F-0007 Onion architecture setup

---
+++ src/Services/Catalog/Catalog.Core.Domain/Entities/Brand.cs
@@ -0,0 +1,9 @@
+﻿using Catalog.Core.Domain.Common;
+
+namespace Catalog.Core.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.Core.Domain/Entities/CatalogType.cs
@@ -0,0 +1,9 @@
+﻿using Catalog.Core.Domain.Common;
+
+namespace Catalog.Core.Domain.Entities
+{
+    public class CatalogType : EntityBase
+    {
+        public string Name { get; set; }
+    }
+}
