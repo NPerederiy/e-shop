3ea064bfa5dcb82f3300943e58ca7f2d9ac8d217
NPerederiy <nick.perederiy@gmail.com>
2020-11-16 02:48:45 +0200

    F-0009 Configured entity framework

--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -1,18 +1,29 @@
+using Catalog.Core.Application.DependencyInjection;
+using Catalog.Infrastructure.Persistence.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Catalog.Core.Application.DependencyInjection;
 
 namespace Catalog.API
 {
     public class Startup
     {
+        private readonly IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddApplication();
+            services.AddPersistence(_config);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ src/Services/Catalog/Catalog.Infrastructure.Persistence/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using Catalog.Core.Application.Interfaces;
+using Catalog.Core.Domain.Entities;
+using Catalog.Infrastructure.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog.Infrastructure.Persistence.Contexts
+{
+    public class ApplicationDbContext : DbContext, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<CatalogItem> CatalogItems { get; set; }
+        public DbSet<CatalogType> CatalogTypes { get; set; }
+
+        public Task<int> SaveChangesAsync()
+        {
+            return base.SaveChangesAsync();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new BrandConfig());
+            modelBuilder.ApplyConfiguration(new CatalogTypeConfig());
+            modelBuilder.ApplyConfiguration(new CatalogItemConfig());
+        }
+    }
+}
