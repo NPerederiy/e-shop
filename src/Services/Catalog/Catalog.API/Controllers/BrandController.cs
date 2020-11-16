8f98dd2f4267845a7db1d2edcbda5d672841c20c
NPerederiy <nick.perederiy@gmail.com>
2020-11-16 23:07:20 +0200

    F-0010 Added API controllers

---
+++ src/Services/Catalog/Catalog.API/Controllers/BrandController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using Catalog.Core.Application.Features.BrandFeatures.Commands;
+using Catalog.Core.Application.Features.BrandFeatures.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    public class BrandController : ApiControllerBase
+    {
+        /// <summary>
+        /// Gets all brands.
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await Mediator.Send(new GetAllBrandsQuery()));
+        }
+
+        /// <summary>
+        /// Gets brand by id.
+        /// </summary>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok(await Mediator.Send(new GetBrandByIdQuery { Id = id }));
+        }
+
+        /// <summary>
+        /// Creates a new brand.
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateBrandCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        /// <summary>
+        /// Updates brand based on id.
+        /// </summary>
+        [HttpPut("[action]")]
+        public async Task<IActionResult> Update(Guid id, UpdateBrandCommand command)
+        {
+            if (id != command.Id) return BadRequest();
+
+            return Ok(await Mediator.Send(command));
+        }
+
+        /// <summary>
+        /// Deletes brand based on id.
+        /// </summary>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok(await Mediator.Send(new DeleteBrandCommand { Id = id }));
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Controllers/TypeController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using Catalog.Core.Application.Features.TypeFeatures.Commands;
+using Catalog.Core.Application.Features.TypeFeatures.Queries;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    public class TypeController : ApiControllerBase
+    {
+        /// <summary>
+        /// Gets all catalog types.
+        /// </summary>
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok(await Mediator.Send(new GetAllCatalogTypesQuery()));
+        }
+
+        /// <summary>
+        /// Gets catalog type by id.
+        /// </summary>
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            return Ok(await Mediator.Send(new GetCatalogTypeByIdQuery { Id = id }));
+        }
+
+        /// <summary>
+        /// Creates a new catalog type.
+        /// </summary>
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateCatalogTypeCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        /// <summary>
+        /// Updates catalog type based on id.
+        /// </summary>
+        [HttpPut("[action]")]
+        public async Task<IActionResult> Update(Guid id, UpdateCatalogTypeCommand command)
+        {
+            if (id != command.Id) return BadRequest();
+
+            return Ok(await Mediator.Send(command));
+        }
+
+        /// <summary>
+        /// Deletes catalog type based on id.
+        /// </summary>
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok(await Mediator.Send(new DeleteCatalogTypeCommand { Id = id }));
+        }
+    }
+}
