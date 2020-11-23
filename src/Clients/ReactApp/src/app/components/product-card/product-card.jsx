aeabb988b53706d999e9228d3c32a11e5cc99fb3
NPerederiy <nick.perederiy@gmail.com>
2020-11-23 02:29:29 +0200

    F-0022 Added Material-UI

--- src/Clients/ReactApp/src/app/components/product-card/product-card.jsx
+++ src/Clients/ReactApp/src/app/components/product-card/product-card.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 
 import defaultImage from '../../../assets/camera-100.png';
 import './product-card.scss';
@@ -6,24 +9,33 @@ import './product-card.scss';
 const ProductCard = (props) => {
     const { name, price, img, discount } = props;
 
+    const currencyCode = 'UAH'; // TODO: take the code from a server
+    const addToCartLabel = 'Add to cart'; // TODO: take the label from a localization dictionary
+
     return (
-        <div id='product-card-container'>
+        <Box id='product-card-container'>
             {img ? (
-                    <img src={img} className='product-card-image' alt='product-image' />
-                ) : (
+                <img src={img} className='product-card-image' alt='product-image' />
+            ) : (
                     <img src={defaultImage} className='product-card-image placeholder' alt='image-is-missing' />
                 )}
             <p className='product-card-header'>{name}</p>
             {discount ? (
-                <div>
-                    <span className='product-card-price'>{discount}</span>
-                    <span className='product-card-price-old'>{price}</span>
-                </div>
+                <Box>
+                    <Box component="span" className='product-card-price'>{discount}</Box>
+                    <Box component="span" className='product-card-price-old'>{price}</Box>
+                    <Box component="span" className='product-card-currency-code'>{currencyCode}</Box>
+                </Box>
             ) : (
-                    <span className='product-card-price'>{price}</span>
-                )}
-            <button>Add to cart</button>    
-        </div>
+                <Box>
+                    <Box component="span" className='product-card-price'>{price}</Box>
+                    <Box component="span" className='product-card-currency-code'>{currencyCode}</Box>
+                </Box>
+            )}
+            <Button variant='contained' color='primary' size='large' startIcon={<AddShoppingCartIcon />}>
+                {addToCartLabel}
+            </Button>
+        </Box>
     );
 }
 
--- src/Clients/ReactApp/src/app/pages/catalog/catalog.jsx
+++ src/Clients/ReactApp/src/app/pages/catalog/catalog.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
 
 import { Loader } from '../../components/loader/loader';
 import { ProductCard } from '../../components/product-card/product-card';
@@ -10,55 +11,56 @@ const Catalog = () => {
     // TODO: replace with api call
     const items = [
         {
-            id:0, 
-            name:'Lorem Ipsum Dolor Sit Amet', 
-            price:'19999',
-            img:'https://i.citrus.ua/imgcache/size_180/uploads/shop/0/2/02b3617419017a1ca52087e5a595ddf9.jpg', 
-            discount:'17999'
+            id: 0,
+            name: 'Lorem Ipsum Dolor Sit Amet',
+            price: '19999',
+            img: 'https://i.citrus.ua/imgcache/size_180/uploads/shop/0/2/02b3617419017a1ca52087e5a595ddf9.jpg',
+            discount: '17999'
         },
         {
-            id:1, 
-            name:'Lorem Ipsum Dolor Sit Amet', 
-            price:'25999', 
-            img:'https://i.citrus.ua/imgcache/size_180/uploads/shop/0/2/02b3617419017a1ca52087e5a595ddf9.jpg', 
-            discount:'28000'},
-        {
-            id:2, 
-            name:'Lorem Ipsum Dolor Sit Amet', 
-            price:'18650',
-            img:'https://i.citrus.ua/imgcache/size_180/uploads/shop/0/2/02b3617419017a1ca52087e5a595ddf9.jpg', 
+            id: 1,
+            name: 'Lorem Ipsum Dolor Sit Amet',
+            price: '29000',
+            img: 'https://i.citrus.ua/imgcache/size_180/uploads/shop/0/2/02b3617419017a1ca52087e5a595ddf9.jpg',
+            discount: '25999'
         },
         {
-            id:3, 
-            name:'Lorem Ipsum Dolor Sit Amet', 
-            price:'19999', 
-            img:'https://i.citrus.ua/imgcache/size_180/uploads/shop/0/2/02b3617419017a1ca52087e5a595ddf9.jpg', 
-            discount:'17999'
+            id: 2,
+            name: 'Lorem Ipsum Dolor Sit Amet',
+            price: '18650',
+            img: 'https://i.citrus.ua/imgcache/size_180/uploads/shop/0/2/02b3617419017a1ca52087e5a595ddf9.jpg',
         },
         {
-            id:4, 
-            name:'Lorem Ipsum Dolor Sit Amet', 
-            price:'25999', 
-            img:'https://i.citrus.ua/imgcache/size_180/uploads/shop/0/2/02b3617419017a1ca52087e5a595ddf9.jpg', 
-            discount:'28000'
+            id: 3,
+            name: 'Lorem Ipsum Dolor Sit Amet',
+            price: '19999',
+            img: 'https://i.citrus.ua/imgcache/size_180/uploads/shop/0/2/02b3617419017a1ca52087e5a595ddf9.jpg',
+            discount: '17999'
         },
         {
-            id:5, 
-            name:'Lorem Ipsum Dolor Sit Amet', 
-            price:'18650'
+            id: 4,
+            name: 'Lorem Ipsum Dolor Sit Amet',
+            price: '28000',
+            img: 'https://i.citrus.ua/imgcache/size_180/uploads/shop/0/2/02b3617419017a1ca52087e5a595ddf9.jpg',
+            discount: '25999'
+        },
+        {
+            id: 5,
+            name: 'Lorem Ipsum Dolor Sit Amet',
+            price: '18650'
         }
     ];
 
     return (
-        <div id='catalog-page'>
+        <Box id='catalog-page'>
             {items ? (
-                <div id='catalog-container'>
+                <Box id='catalog-container'>
                     {items.map(item => (<ProductCard key={item.id} {...item} />))}
-                </div>
+                </Box>
             ) : (
                     <Loader message='Loading components...' />
                 )}
-        </div>
+        </Box>
     );
 }
 
