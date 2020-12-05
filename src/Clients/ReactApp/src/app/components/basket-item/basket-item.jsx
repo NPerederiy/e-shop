d7109cd313f3134d0a353890a3b6560324fed525
NPerederiy <nick.perederiy@gmail.com>
2020-12-05 23:59:13 +0200

    F-0031 Added a basket popper

---
+++ src/Clients/ReactApp/src/app/components/basket-item/basket-item.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+
+import './basket-item.scss';
+import defaultImage from '../../../assets/camera-100.png';
+
+const BasketItem = ({
+    className,
+    name,
+    price,
+    img,
+    discount,
+    currencyCode = 'UAH',
+}) => {
+    return (
+        <Box id='basket-item-container' className={className}>
+            <img
+                src={img ?? defaultImage}
+                className={img ?
+                    'basket-item-image interactive' :
+                    'basket-item-image placeholder interactive'}
+                alt='product'
+            />
+            <Box className='basket-item-description'>
+            <p className='basket-item-header'>
+                {name}
+            </p>
+            {discount ? (
+                <Box>
+                    <Box component="span" className='basket-item-price'>{discount}</Box>
+                    <Box component="span" className='basket-item-price-old'>{price}</Box>
+                    <Box component="span" className='basket-item-currency-code'>{currencyCode}</Box>
+                </Box>
+            ) : (
+                    <Box>
+                        <Box component="span" className='basket-item-price'>{price}</Box>
+                        <Box component="span" className='basket-item-currency-code'>{currencyCode}</Box>
+                    </Box>
+                )}
+            </Box>
+        </Box>
+    )
+}
+
+export default BasketItem;
