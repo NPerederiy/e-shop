e341bde241eba765b46a5aa9bd2c85f0921f3d82
NPerederiy <nick.perederiy@gmail.com>
2020-12-20 19:45:35 +0200

    F-0040 Updated controllers

--- src/Services/Basket/Basket.API/Controllers/CheckoutController.cs
+++ src/Services/Basket/Basket.API/Controllers/CheckoutController.cs
@@ -1,47 +1,118 @@
 ﻿using System;
+using System.Threading.Tasks;
 using Basket.Domain.Entities;
+using Basket.Persistence.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Basket.API.Controllers
 {
     public class CheckoutController : ApiControllerBase
     {
+        private readonly IApplicationDbContext _context;
+
+        public CheckoutController(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         /// <summary>
         /// Gets basket checkout by buyer id.
         /// </summary>
         [HttpGet("{id}")]
-        public IActionResult GetById(Guid id)
+        public async Task<IActionResult> GetById(Guid id)
         {
-            return Ok("get checkout " + id);
+            try
+            {
+                var payload = await _context.BasketCheckouts
+                    .FirstOrDefaultAsync(x => x.BuyerId == id);
+
+                if (payload == null)
+                {
+                    return NotFound("Item not found.");
+                }
+
+                return Ok(payload);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         /// <summary>
         /// Creates a basket checkout.
         /// </summary>
         [HttpPost]
-        public IActionResult Create(BasketCheckout checkout)
+        public async Task<IActionResult> Create(BasketCheckout checkout)
         {
-            return Ok("create checkout ");
+            try
+            {
+                await _context.BasketCheckouts.AddAsync(checkout);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok(checkout.BuyerId);
         }
 
         /// <summary>
         /// Updates basket checkout based on the buyer id.
         /// </summary>
         [HttpPut("{id}")]
-        public IActionResult Update(Guid id, BasketCheckout checkout)
+        public async Task<IActionResult> Update(Guid id, BasketCheckout checkout)
         {
             if (id != checkout.BuyerId) return BadRequest();
 
-            return Ok("update checkout " + id);
+            try
+            {
+                var payload = await _context.BasketCheckouts
+                    .FirstOrDefaultAsync(x => x.BuyerId == id);
+
+                if (payload == null)
+                {
+                    return NotFound("Item not found.");
+                }
+
+                _context.BasketCheckouts.Update(checkout);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            
+            return NoContent();
         }
 
         /// <summary>
         /// Deletes basket checkout based on the buyer id.
         /// </summary>
         [HttpDelete("{id}")]
-        public IActionResult Delete(Guid id)
+        public async Task<IActionResult> Delete(Guid id)
         {
-            return Ok("delete checkout " + id);
+            try
+            {
+                var payload = await _context.BasketCheckouts
+                    .FirstOrDefaultAsync(x => x.BuyerId == id);
+
+                if (payload == null)
+                {
+                    return NotFound("Item not found.");
+                }
+
+                _context.BasketCheckouts.Remove(payload);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return NoContent();
         }
     }
 }
--- src/Services/Basket/Basket.API/Controllers/ItemController.cs
+++ src/Services/Basket/Basket.API/Controllers/ItemController.cs
@@ -1,56 +1,138 @@
 ﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
 using Basket.Domain.Entities;
+using Basket.Persistence.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Basket.API.Controllers
 {
     public class ItemController : ApiControllerBase
     {
+        private readonly IApplicationDbContext _context;
+
+        public ItemController(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         /// <summary>
-        /// Gets all basket items.
+        /// Gets all basket items by buyer's id.
         /// </summary>
         [HttpGet]
-        public IActionResult GetAll()
+        public IActionResult GetAll(Guid buyerId)
         {
-            return Ok("get all");
+            try
+            {
+                var payload = _context.BasketItems
+                    .Where(x => x.BuyerId == buyerId);
+
+                return Ok(payload);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         /// <summary>
         /// Gets basket item by id.
         /// </summary>
         [HttpGet("{id}")]
-        public IActionResult GetById(Guid id)
+        public async Task<IActionResult> GetById(Guid id)
         {
-            return Ok("get item " + id);
+            try
+            {
+                var payload = await _context.BasketItems
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                if (payload == null)
+                {
+                    return NotFound("Item not found.");
+                }
+
+                return Ok(payload);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
         }
 
         /// <summary>
         /// Creates a new basket item.
         /// </summary>
         [HttpPost]
-        public IActionResult Create(BasketItem item)
+        public async Task<IActionResult> Create(BasketItem item)
         {
-            return Ok("create item ");
+            try
+            {
+                await _context.BasketItems.AddAsync(item);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return Ok(item.Id);
         }
 
         /// <summary>
         /// Updates basket item based on id.
         /// </summary>
         [HttpPut("{id}")]
-        public IActionResult Update(Guid id, BasketItem item)
+        public async Task<IActionResult> Update(Guid id, BasketItem item)
         {
-            if (id != item.Id) return BadRequest();
+            if (id != item.Id) return BadRequest("Missing the basket item id.");
 
-            return Ok("update item " + id);
+            try
+            {
+                var payload = await _context.BasketItems
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                if (payload == null)
+                {
+                    return NotFound("Item not found.");
+                }
+
+                _context.BasketItems.Update(item);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                BadRequest(ex.Message);
+            }
+
+            return NoContent();
         }
 
         /// <summary>
         /// Deletes basket item based on id.
         /// </summary>
         [HttpDelete("{id}")]
-        public IActionResult Delete(Guid id)
+        public async Task<IActionResult> Delete(Guid id)
         {
-            return Ok("delete item " + id);
+            try
+            {
+                var payload = await _context.BasketItems
+                    .FirstOrDefaultAsync(x => x.Id == id);
+
+                if (payload == null)
+                {
+                    return NotFound("Item not found.");
+                }
+
+                _context.BasketItems.Remove(payload);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+            return NoContent();
         }
     }
 }
