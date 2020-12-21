e19321bbc8cc37de3b83723f8b79ea6c2078856b
NPerederiy <nick.perederiy@gmail.com>
2020-12-21 20:11:45 +0200

    F-0042 Configuring the Identity server (draft)

---
+++ src/Services/Identity/Identity.API/Models/SignInData.cs
@@ -0,0 +1,15 @@
+﻿namespace Identity.API.Models
+{
+    public class SignInData
+    {
+        /// <summary>
+        /// User email.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Password hash.
+        /// </summary>
+        public string Password { get; set; }
+    }
+}
